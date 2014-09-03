f4d12437987e47391737bc86f49713ba615eded6
Alexander Bakken <alex.h.bakken@gmail.com>
2014-09-04 00:53:45 +0200

    Initializing...  uber project

---
+++ LocalProject/Project I/src/program.java
@@ -0,0 +1,11 @@
+
+public class program {
+	
+	public static void main(String[] args) {
+		
+		String name = "Alexander";
+		int age = 22; 
+		myClass run = new myClass(name, age);
+		System.out.println(run.toString());
+	}
+}
